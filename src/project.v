2b13b34347d3ac15b6327c135bad9d87e9b280a2
Sriram S <sriram.s0011@gmail.com>
2025-09-09 13:59:58 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,23 +5,25 @@
 
 `default_nettype none
 
-module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+module tt_um_lfsr_parity (
+    input wire clk,
+    input wire rst,
+    output reg [7:0] lfsr_out
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+reg [6:0] lfsr;
+wire feedback = lfsr[6] ^ lfsr[5];  // taps at bit 6 and 5
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+always @(posedge clk or posedge rst) begin
+    if (rst)
+        lfsr <= 7'b0000001;
+    else
+        lfsr <= {lfsr[5:0], feedback};
+end
+
+always @(*) begin
+    lfsr_out[6:0] = lfsr;
+    lfsr_out[7]   = ~(^lfsr);  // even parity
+end
 
 endmodule
